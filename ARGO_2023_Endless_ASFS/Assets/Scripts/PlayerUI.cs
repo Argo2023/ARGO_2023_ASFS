b23ebaaec64e766f7c13a05bcc3c8c7d56bbd118
Adam Mcguigan <58327044+AdamMcguigan@users.noreply.github.com>
2023-02-16 11:48:44 +0000

    AD 60 changing network manager - OG (#9)
    * Made some progress today
    
    Changed the network manager to the new lobby manager, also made some progress with the ready up and lobby situations
    
    * Fixed the Networking
    
    Thanks to Andrew for some help
    
    * AD-77 Finished
    
    Changed around the lobby screen as it wasnt working in the previous layout with the new mirror network room manager
    
    * AD-83
    
    Created a button in the lobby where a player can choose to join the game via inputting the IP of the host
    
    * Added missing assembly reference after merge with master

---
+++ ARGO_2023_Endless_ASFS/Assets/Scripts/PlayerUI.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Mirror.Examples.Basic
+{
+    public class PlayerUI : MonoBehaviour
+    {
+        public Player player;
+
+        [Header("Player Components")]
+        public Image image;
+
+        [Header("Child Text Objects")]
+        public Text playerNameText;
+        public Button buttonToReadyUp;
+
+        // Sets a highlight color for the local player
+        public void SetLocalPlayer()
+        {
+            // add a visual background for the local player in the UI
+            image.color = new Color(1f, 1f, 1f, 0.1f);
+        }
+
+        // This value can change as clients leave and join
+        public void OnPlayerNumberChanged(byte newPlayerNumber)
+        {
+            playerNameText.text = string.Format("Player {0:00}", newPlayerNumber);
+        }
+
+        // Random color set by Player::OnStartServer
+        public void OnPlayerColorChanged(Color32 newPlayerColor)
+        {
+            playerNameText.color = newPlayerColor;
+        }
+
+        public void SetPlayerReady()
+        {
+            player.GetComponent<NetworkRoomPlayer>().CmdChangeReadyState(true);
+            player.enabled = false;
+            buttonToReadyUp.gameObject.SetActive(false);
+        }
+    }
+}
