579ff247e04ebc4c6e002b8c488e9349f4e3a00a
SeanWhelan117 <57904168+SeanWhelan117@users.noreply.github.com>
2023-02-17 13:38:52 +0000

    Ad 34 scene combination sprint2 (#10)
    * AD-71 Finished, AD-70 Has a bug that needs to be resolved
    
    * AD-65
    
    * AD-62
    
    * AD-70 Getting the platforms that are currently on the screen
    
    * AD-63
    
    * AD-64
    
    * AD-70 Jumping mechanism finished, AI is jumping around finding the next platform, and choosing between jumping on the platform or attacking the player
    
    * AD-78, AD-79, AD-80, AD-81 First three finished, Starting AD-81
    
    * Commenting all of the AI functions created
    
    * added spawining from the top to the powerups
    
    * Fixed camera shake on SP
    
    * Adding stuff together
    
    * added lots of sprites to try make it more presentable. Added newer AI version to main scene
    
    Bug with Filips UI reload
    
    * Added health sprite to the generic powerup, added powerups
    
    ---------
    
    Co-authored-by: SasaKuzmanovic <janisamxdgamingstudio@gmail.com>
    Co-authored-by: FilipRusiecki <57904846+FilipRusiecki@users.noreply.github.com>

--- ARGO_2023_Endless_ASFS/Assets/Scripts/AIScript.cs
+++ ARGO_2023_Endless_ASFS/Assets/Scripts/AIScript.cs
@@ -31,13 +31,23 @@ enum Actions
 public class AIScript : MonoBehaviour
 {
     public GameObject enemy;
+    public GameObject platforms;
+    List<Actions> actions = new List<Actions>();
+    List<GameObject> allPlatforms = new List<GameObject>();
+    List<GameObject> allGameObjects = new List<GameObject>();
+    AIState state = AIState.NOTHING;
+
     public string transfer = "";
     public bool jumpTrigger = false;
-    public bool finished = false;
+    public bool grounded = true;
+    public bool finished = true;
+    public bool wee = false;
     public Rigidbody2D rb;
+    float tempBest = float.MaxValue;
     public float speed = 1.0f;
-    List<Actions> actions = new List<Actions>();
-    AIState state = AIState.NOTHING;
+    public int ID = 0;
+
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -47,30 +57,52 @@ public class AIScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        state = getEntityState();
+
         setAIAction();
 
-        //Debug.Log(getEntityState());
-        if (getEntityState() == AIState.ATTACKING)
+        if (state == AIState.ATTACKING)
         {
             attackingExecution(actions);
         }
 
-        if (getEntityState() == AIState.CHASING)
+        if (state == AIState.CHASING)
         {
             chasingExecution(actions);
         }
 
-        if (getEntityState() == AIState.EVADING)
+        if (state == AIState.EVADING)
         {
             evadingExecution(actions);
         }
 
-        if (getEntityState() == AIState.IDLE)
+        if (state == AIState.IDLE)
         {
             idleExecution(actions);
         }
 
-       // Debug.Log(rb.velocity);
+        if (Input.touchCount > 0)
+        {
+            GetComponent<HealthScript>().baseHealth = 39;
+
+            getPlatforms();
+
+            var tempBest = 0.0f;
+            for (int i = 0; i < allPlatforms.Count; i++)
+            {
+                var distance = Vector2.Distance(transform.position, allPlatforms[i].transform.position);
+
+                if (distance < tempBest)
+                {
+                    tempBest = distance;
+                    Debug.Log("ID: " + i.ToString());
+                }
+            }
+        }
+
+
+        //Debug.Log(rb.velocity);
     }
 
 
@@ -118,26 +150,30 @@ public class AIScript : MonoBehaviour
             // add actions for the player to do while attacking
         }
 
-        if (state == AIState.CHASING && finished != false)
+        if (state == AIState.CHASING && finished == true)
         {
+            Debug.Log("CHASING");
+            actions.Clear();
             actions.Add(Actions.MOVE);
             actions.Add(Actions.JUMP);
             actions.Add(Actions.TILT);
-            Debug.Log(syncActions());
             finished = false;
         }
 
         if (state == AIState.EVADING)
         {
+            actions.Clear();
+            actions.Add(Actions.JUMP);
             // add actions for the player to do while evading
         }
 
         if (state == AIState.IDLE)
         {
+            Debug.Log("IDLE");
+            actions.Clear();
             actions.Add(Actions.MOVE);
             actions.Add(Actions.JUMP);
-
-            
+            finished = false;
         }
     }
 
@@ -149,10 +185,18 @@ public class AIScript : MonoBehaviour
     /// <param name="collision"></param>
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag("wall"))
+        if (collision.gameObject.CompareTag("Player"))
         {
             jumpTrigger = true;
             speed = -speed;
+        }
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            grounded = true;
+        }
+        if (collision.gameObject.CompareTag("Platform"))
+        {
+            grounded = true;
         }
     }
 
@@ -171,6 +215,7 @@ public class AIScript : MonoBehaviour
         if (t_actions[0] == Actions.JUMP)
         {
             rb.AddForce(Vector2.up * 5.0f, ForceMode2D.Impulse);
+            grounded = false;
             t_actions.RemoveAt(0);
         }
         if (jumpTrigger)
@@ -182,7 +227,79 @@ public class AIScript : MonoBehaviour
 
     void evadingExecution(List<Actions> t_actions)
     {
+        if (t_actions[0] == Actions.MOVE)
+        {
+            if (!(transform.position.x - enemy.transform.position.x <= 4.0f) ||
+                enemy.transform.position.x - transform.position.x >= 4.0f)
+            {
+                if (enemy.transform.position.x < transform.position.x)
+                    transform.position += Vector3.right * speed * Time.deltaTime;
+                if (enemy.transform.position.x > transform.position.x)
+                    transform.position -= Vector3.right * speed * Time.deltaTime;
+            }
+        }
 
+        if (t_actions[0] == Actions.JUMP)
+        {
+            getPlatforms();
+            
+            /// Gets the closest platform to the AI the script is running on.
+            /// Tries to get to the player, by jumping on the platforms
+
+            for (int i = 0; i < allPlatforms.Count; i++)
+            {
+                var distance = Vector2.Distance(transform.position, allPlatforms[i].transform.position);
+
+                if (distance < tempBest)
+                {
+                    tempBest = distance;
+                    Debug.Log("ID: " + i.ToString());
+                    ID = i;
+                }
+            }
+
+            float distanceToPlayer = Vector2.Distance(transform.position, enemy.transform.position);
+
+            Debug.Log("Distance to Player: " + distanceToPlayer.ToString());
+            Debug.Log("Closest platform: " + tempBest.ToString());
+
+            /// If the player is on the ground, it looks if the player is closer to the AI or is the
+            /// platform closer to the AI, which will decide if the AI jumps on the platform.
+            /// It runs a coroutine that after 0.2 of a second pushes the AI in a direction to
+            /// Mimic player movement trying to climb a platform
+            if (grounded)
+            {
+                if (tempBest < distanceToPlayer)
+                {
+                    if (transform.position.x - allPlatforms[ID].transform.position.x < 0)
+                    {
+                        grounded = false;
+                        rb.AddForce(Vector2.up * 10.0f, ForceMode2D.Impulse);
+
+                        StartCoroutine(platformJumpRight());
+                    }
+                    else
+                    {
+                        grounded = false;
+                        rb.AddForce(Vector2.up * 10.0f, ForceMode2D.Impulse);
+
+                        StartCoroutine(platformJumpLeft());
+                    }
+                }
+            }
+        }
+    }
+
+    IEnumerator platformJumpRight()
+    {
+        yield return new WaitForSeconds(0.2f);
+        rb.AddForce(Vector2.right * 2.0f, ForceMode2D.Impulse);   
+    }
+
+    IEnumerator platformJumpLeft()
+    {
+        yield return new WaitForSeconds(0.2f);
+        rb.AddForce(Vector2.left * 2.0f, ForceMode2D.Impulse);
     }
 
     void chasingExecution(List<Actions> t_actions)
@@ -190,10 +307,9 @@ public class AIScript : MonoBehaviour
         //Debug.Log("Im here");
         if (t_actions[0] == Actions.MOVE)
         {
-            if (!(transform.position.x - enemy.transform.position.x <= 1.0f) ||
-                enemy.transform.position.x - transform.position.x >= 1.0f)
+            if (!(transform.position.x - enemy.transform.position.x <= 4.0f) ||
+                enemy.transform.position.x - transform.position.x >= 4.0f)
             {
-                Debug.Log(enemy.transform.position.x - transform.position.x);
                 if (enemy.transform.position.x < transform.position.x)
                     transform.position -= Vector3.right * speed * Time.deltaTime;
                 if(enemy.transform.position.x > transform.position.x)
@@ -207,29 +323,41 @@ public class AIScript : MonoBehaviour
         
         if (t_actions[0] == Actions.JUMP)
         {
-            //Debug.Log("HELLLOOOOO");
-            rb.AddForce(Vector2.up * 2.0f, ForceMode2D.Impulse);
-            t_actions.RemoveAt(0);
+            if (grounded)
+            {
+                //Debug.Log("HELLLOOOOO");
+                rb.AddForce(Vector2.up * 6.0f, ForceMode2D.Impulse);
+                grounded = false;
+                t_actions.RemoveAt(0);
+                wee = true;
+            }
+            else
+            {
+                t_actions.RemoveAt(0);
+            }
+            
         }
 
         if (t_actions[0] == Actions.TILT)
         {
-            if (enemy.transform.position.x < transform.position.x)
+            if ((enemy.transform.position.x > transform.position.x && wee == true) || grounded == true)
             {
-                //Debug.Log("I TILT");
-                rb.AddForce(Vector2.right * 1.0f, ForceMode2D.Impulse);
+                rb.AddForce(Vector2.right * 10.0f, ForceMode2D.Impulse);
                 t_actions.RemoveAt(0);
                 finished = true;
+                wee = false;
+
             }
 
-            if (enemy.transform.position.x > transform.position.x)
+            if (enemy.transform.position.x < transform.position.x && wee == true || grounded == true)
             {
-                //Debug.Log("I TILT");
-                rb.AddForce(Vector2.left * 1.0f, ForceMode2D.Impulse);
+                rb.AddForce(Vector2.left * 10.0f, ForceMode2D.Impulse);
                 t_actions.RemoveAt(0);
                 finished = true;
+                wee = false;
             }
         }
+        // add shooting etc for chase mode
 
     }
 
@@ -242,10 +370,221 @@ public class AIScript : MonoBehaviour
     {
         for (int i = 0; i < actions.Count; i++)
         {
-            transfer += actions[i].ToString() + ", ";
+            transfer += actions[i].ToString() + ",";
         }
 
         return transfer;
     }
 
+    /// <summary>
+    /// Method for finding all of the platforms that are currently spawned in the scene.
+    /// Currently does not handle deleting from the list, when new platforms are in, and old ones are despawned
+    /// </summary>
+
+    GameObject[] getPlatforms()
+    {
+        checkForDeadPlatforms();
+
+        bool maybeNotIn = false;
+        var objects = GameObject.FindGameObjectsWithTag("Platform");
+         
+        if (allPlatforms.Count == 0)
+        {
+            for(int i = 0; i < objects.Length; i++)
+            {
+                allPlatforms.Add(objects[i]);
+            }
+        }
+
+        for (int i = 0; i < objects.Length; i++)
+        {
+            for (int j = 0; j < allPlatforms.Count;j++)
+            {
+                maybeNotIn = false;
+                if (objects[i].Equals(allPlatforms[j]))
+                {
+                    maybeNotIn = true;
+                    break;
+                }
+            }
+
+            if (maybeNotIn == false)
+            {
+                allPlatforms.Add(objects[i]);
+            }
+        }
+
+        return objects;
+    }
+
+    void checkForDeadPlatforms()
+    {
+        for (int i = 0; i < allPlatforms.Count; i++)
+        {
+            if (allPlatforms[i] == null)
+            {
+                allPlatforms.RemoveAt(i);
+            }
+        }
+    }
+
+    /// <summary>
+    /// Returns a value that will be used in decision making for the AI
+    /// Gathers distances from all of the relevant GameObjects and decides on danger of each Entity
+    /// The lower the weight, the higher the risk AI is currently in
+    /// </summary>
+    /// <returns></returns>
+    float evaluateDistance()
+    { 
+
+        int IDOfEntity = 0;
+        var players = GameObject.FindGameObjectsWithTag("Player");
+        var platforms = getPlatforms();
+        var otherAI = GameObject.FindGameObjectsWithTag("AI");
+
+
+        fillListWithPlayers(players);
+        fillListWithPlatforms(platforms);
+        fillListWithAI(otherAI);
+
+        int numberOfEntities = players.Length + platforms.Length + otherAI.Length;
+
+        float closestValue = float.MaxValue;
+
+        for (int i = 0; i < numberOfEntities; i++)
+        {
+            float distance = Vector2.Distance(transform.position, allGameObjects[i].transform.position);
+
+            if (distance < closestValue)
+            {
+                closestValue = distance;
+                IDOfEntity = i;
+                Debug.Log("Closest Entity ID: " + IDOfEntity.ToString());
+            }
+        }
+
+        if (allGameObjects[IDOfEntity].tag == "Platform")
+        {
+            return closestValue * 50;
+        }
+
+        if (allGameObjects[IDOfEntity].tag == "Player")
+        {
+            return closestValue / 50;
+        }
+
+        if (allGameObjects[IDOfEntity].tag == "AI")
+        {
+            return closestValue;
+        }
+
+
+        return 0.0f;
+        
+    }
+
+    void fillListWithPlayers(GameObject[] t_players)
+    {
+        for (int i = 0; i < t_players.Length; i++)
+        {
+            allGameObjects.Add(t_players[i]);
+        }
+    }
+
+    void fillListWithPlatforms(GameObject[] t_platforms)
+    {
+        for (int i = 0; i < t_platforms.Length; i++)
+        {
+            allGameObjects.Add(t_platforms[i]);
+        }
+    }
+
+    void fillListWithAI(GameObject[] t_AI)
+    {
+        for (int i = 0; i < t_AI.Length; i++)
+        {
+            allGameObjects.Add(t_AI[i]);
+        }
+    }
+
+    /// REFACTOR THIS CODE
+
+
+    //int getClosestEntity(ref List<GameObject> t_allObjects)
+    //{
+    //    int IDOfEntity = 0;
+    //    float closestValue = float.MaxValue;
+
+    //    for (int i = 0; i < t_allObjects.Count; i++)
+    //    {
+    //        float distance = Vector2.Distance(transform.position, t_allObjects[i].transform.position);
+
+    //        if (distance < closestValue)
+    //        {
+    //            closestValue = distance;
+    //            IDOfEntity = i;
+    //            Debug.Log("Closest Entity ID: " + IDOfEntity.ToString());
+    //        }
+    //    }
+
+    //    return IDOfEntity;
+    //}
+
+    ///Takes the AI health and uses it in deciding what Aciton should the AI Take
+    float evaluateHealth()
+    { 
+        return GetComponent<HealthScript>().baseHealth; 
+    }
+
+    /// <summary>
+    /// Evaluates in how big of a risk AI is in. If it is really close to the Entity, It will return a low
+    /// value, indication that the AI is in HIGH RISK.
+    /// </summary>
+    /// <returns></returns>
+    float evaluateShooting()
+    {
+        float closestValue = float.MaxValue;
+
+        for (int i = 0; i < allGameObjects.Count; i++)
+        {
+            float distance = Vector2.Distance(transform.position, allGameObjects[i].transform.position);
+
+            if (distance < closestValue)
+            {
+                closestValue = distance;
+            }
+        }
+
+        if (closestValue > 0 && closestValue < 2.0f)
+        {
+            return closestValue / 100;
+        }
+
+        if (closestValue >= 2.0f && closestValue < 10.0f)
+        {
+            return closestValue * 2;
+        }
+
+        if (closestValue > 10)
+        {
+            return closestValue * 20;
+        }
+
+        return 0.0f;
+    }
+
+    /// <summary>
+    /// Based on the results from the evaluations, this will use a neural network to decide what decision
+    /// it should make.
+    /// </summary>
+    void choosingBehaviour()
+    {
+        float weight = 0.0f;
+
+        weight = evaluateDistance() + evaluateHealth() + evaluateShooting();
+
+
+    }
+
+    
 }
