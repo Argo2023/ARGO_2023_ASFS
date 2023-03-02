906465a937dc6fae76a0f4f574ed2722f1b3a544
Adam Mcguigan <58327044+AdamMcguigan@users.noreply.github.com>
2023-03-02 15:36:43 +0000

    AD-88 networking week 2 continued (#16)
    * AD-65
    
    * AD-62
    
    * AD-63
    
    * AD-64
    
    * Made some progress with the New movement system
    
    Had some difficulty getting the new movement system to work, i believe i have it working but i want to run some more tests before i saw its finished. Also fixed an issue with the bullets on the server
    
    * AD-91 - Got the spawn points finished
    
    Created the network start points that will  be used when the players are joining the game, this has now removed all players spawning on top of each other
    
    * Fixed the new movement system when multiple players join
    
    Fixed the joysticks not working when there is more than one player after joining the game
    
    * Made a start on the game loop
    
    Having difficulty getting the platforms to sync across client and server
    
    * making progress with the gameloop
    
    * Fixed the platform syncing
    
    Fixed the issue where the platforms were not syncing on the client side
    
    * AD-129
    
    Fixed the Input Field for the enter IP button should now work properly on mobile
    
    ---------
    
    Co-authored-by: FilipRusiecki <57904846+FilipRusiecki@users.noreply.github.com>

--- ARGO_2023_Endless_ASFS/Assets/Scripts/ChangeIP.cs
+++ ARGO_2023_Endless_ASFS/Assets/Scripts/ChangeIP.cs
@@ -3,15 +3,49 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using Mirror;
+using System.Net;
+using System.Net.Sockets;
+using TMPro;
 
 namespace Mirror.Examples.Basic
 {
     public class ChangeIP : MonoBehaviour
     {
+        public TextMeshProUGUI ipText;
+
+        public void Start()
+        {
+            string ip = "my ip: " + TheLocalIP();    
+            ipText.text = ip;
+        }
+
         public void changeIP()
         {
-            InputField _inputField = this.GetComponent<InputField>();
-            FindObjectOfType<NewNetworkRoomManager>().networkAddress = _inputField.text;
+            TMP_InputField inputF = this.GetComponent<TMP_InputField>();
+            FindObjectOfType<NewNetworkRoomManager>().networkAddress = inputF.text;
+        }
+
+        
+        public void connect()
+        {
+            System.Uri finalIp = new System.Uri("kcp://" + FindObjectOfType<NewNetworkRoomManager>().networkAddress + ":7777");
+            NetworkManager.singleton.StartClient(finalIp);
+        }
+
+        public static string TheLocalIP()
+        {
+            IPHostEntry hostEntry;
+            string localIP = "0.0.0.0";
+            hostEntry = Dns.GetHostEntry(Dns.GetHostName());
+            foreach(IPAddress ip in hostEntry.AddressList)
+            {
+                if(ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    localIP = ip.ToString();
+                    break;
+                }
+            }
+            return localIP;
         }
     }
 
